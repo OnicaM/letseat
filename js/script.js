36c07ad5f3e32be47cafe650232aa1ab32459597
OnicaM <mac.oana92@yahoo.ro>
2019-03-15 04:31:49 +0200

    update website

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-function displayMobileNav(){
+function displayCatNav(){
 	 var element = document.getElementById("categoriesD");
   		element.parentElement.classList.toggle("active");
+}
+function displayMobileNav(){
+	 var element = document.getElementById("body");
+  		element.classList.toggle("js--mobile");
 }
