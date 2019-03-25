203c9eb932441325a65c4acaf1b83796ffb4cd0c
OnicaM <mac.oana92@yahoo.ro>
2019-03-25 08:28:52 +0200

    updates

--- js/script.js
+++ js/script.js
@@ -6,3 +6,32 @@ function displayMobileNav(){
 	 var element = document.getElementById("body");
   		element.classList.toggle("js--mobile");
 }
+
+
+
+
+
+
+window.addEventListener("load", function() {
+	// store tabs variable
+	var myTabs = document.querySelectorAll("ul.tabs_links > li");
+  function myTabClicks(tabClickEvent) {
+		for (var i = 0; i < myTabs.length; i++) {
+			myTabs[i].classList.remove("active");
+		}
+		var clickedTab = tabClickEvent.currentTarget;
+		clickedTab.classList.add("active");
+		tabClickEvent.preventDefault();
+		var myContentPanes = document.querySelectorAll(".tab");
+		for (i = 0; i < myContentPanes.length; i++) {
+			myContentPanes[i].classList.remove("active");
+		}
+		var anchorReference = tabClickEvent.target;
+		var activePaneId = anchorReference.getAttribute("href");
+		var activePane = document.querySelector(activePaneId);
+		activePane.classList.add("active");
+	}
+	for (i = 0; i < myTabs.length; i++) {
+		myTabs[i].addEventListener("click", myTabClicks)
+	}
+});
