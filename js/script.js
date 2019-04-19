349adbc40f600b9bd9d6f35568e69fc31b99ad08
OnicaM <mac.oana92@yahoo.ro>
2019-04-19 05:22:07 +0300

    updates

--- js/script.js
+++ js/script.js
@@ -1,36 +1,51 @@
-function displayCatNav(){
-	 var element = document.getElementById("categoriesD");
-  		element.parentElement.classList.toggle("active");
+function displayCatNav() {
+    var element = document.getElementById("categoriesD");
+    element.parentElement.classList.toggle("active");
 }
-function displayMobileNav(){
-	 var element = document.getElementById("body");
-  		element.classList.toggle("js--mobile");
+function toggleRestaurants() {
+    var pick = document.getElementById("pick");
+    pick.parentElement.classList.toggle("active");
+}
+
+function displayMobileNav() {
+    var element = document.getElementById("body");
+    element.classList.toggle("js--mobile");
 }
 window.addEventListener("load", function() {
-	// store tabs variable
-	var myTabs = document.querySelectorAll("ul.tabs_links > li");
-  function myTabClicks(tabClickEvent) {
-		for (var i = 0; i < myTabs.length; i++) {
-			myTabs[i].classList.remove("active");
-		}
-		var clickedTab = tabClickEvent.currentTarget;
-		clickedTab.classList.add("active");
-		tabClickEvent.preventDefault();
-		var myContentPanes = document.querySelectorAll(".tab");
-		for (i = 0; i < myContentPanes.length; i++) {
-			myContentPanes[i].classList.remove("active");
-		}
-		var anchorReference = tabClickEvent.target;
-		var activePaneId = anchorReference.getAttribute("href");
-		var activePane = document.querySelector(activePaneId);
-		activePane.classList.add("active");
-	}
-	for (i = 0; i < myTabs.length; i++) {
-		myTabs[i].addEventListener("click", myTabClicks)
-		//console.log(myTabs[i]);
-	}
+    // store tabs variable
+    var myTabs = document.querySelectorAll("ul.tabs_links > li");
+
+    function myTabClicks(tabClickEvent) {
+        for (var i = 0; i < myTabs.length; i++) {
+            myTabs[i].classList.remove("active");
+        }
+        var clickedTab = tabClickEvent.currentTarget;
+        clickedTab.classList.add("active");
+        tabClickEvent.preventDefault();
+        var myContentPanes = document.querySelectorAll(".tab");
+        for (i = 0; i < myContentPanes.length; i++) {
+            myContentPanes[i].classList.remove("active");
+        }
+        var anchorReference = tabClickEvent.target;
+        var activePaneId = anchorReference.getAttribute("href");
+        var activePane = document.querySelector(activePaneId);
+        activePane.classList.add("active");
+    }
+    for (i = 0; i < myTabs.length; i++) {
+        myTabs[i].addEventListener("click", myTabClicks)
+        //console.log(myTabs[i]);
+    }
 });
 
+/*Switch login*/
+var login = document.querySelector('.login');
+login.addEventListener('click', function() {
+
+    var registerContainer = document.querySelector('.form_register');
+    var loginContainer = document.querySelector('.form_login');
+    registerContainer.classList.remove('hide');
+    loginContainer.classList.add('hide');
+});
 
 /*Content of table*/
 var tableItems = document.querySelector('.table-items'),
@@ -41,9 +56,9 @@ var tableItems = document.querySelector('.table-items'),
     tableCart = document.querySelector('.order-box_container'),
     totalPriceEl = document.querySelector(".total-price"),
     boxes = document.querySelector('.boxes');
-    send = document.getElementById('send');
-    
-    var tablePasta = document.querySelector('#paste .table-items'),
+send = document.getElementById('send');
+
+var tablePasta = document.querySelector('#paste .table-items'),
     tableSalad = document.querySelector('#salate .table-items'),
     tableSoup = document.querySelector('#ciorbe .table-items'),
     tableMenu = document.querySelector('#meniu .table-items'),
@@ -51,7 +66,7 @@ var tableItems = document.querySelector('.table-items'),
 
 var products = [{
             id: 0,
-            restaurant: "Bistro1",
+            restaurant: "Bistro26",
             type: "traditional",
             category: "salate",
             name: "Salata Caesar",
@@ -60,7 +75,7 @@ var products = [{
         },
         {
             id: 1,
-            restaurant: "Bistro2",
+            restaurant: "Mudy",
             type: "traditional",
             category: "paste",
             name: "Paste Quatro Formagi",
@@ -69,7 +84,7 @@ var products = [{
         },
         {
             id: 2,
-            restaurant: "Bistro3",
+            restaurant: "Gepetto",
             type: "chinezesc",
             category: "burgeri",
             name: "Burger Classic",
@@ -78,7 +93,7 @@ var products = [{
         },
         {
             id: 3,
-            restaurant: "Bistro4",
+            restaurant: "GrillHouse",
             type: "pizza",
             category: "burgeri",
             name: "Burger Double",
@@ -87,7 +102,7 @@ var products = [{
         },
         {
             id: 4,
-            restaurant: "Bistro5",
+            restaurant: "Holand",
             type: "fastfood",
             category: "paste",
             name: "Paste fructe de mare",
@@ -105,51 +120,51 @@ var products = [{
         }
     ],
     productsInCart = [];
-    orderStored = [];
+orderStored = [];
 
 var generateProductList = function() {
     products.forEach(function(item) {
         var productEl = document.createElement("tr");
         productEl.innerHTML = `<td class="product-name"> 
-      							<span class="product-name">${item.name}</span>
-      							<span class="product-description">${item.description}</span>
-      						</td>
-      						<td>
-	      						<span class="product-price">${item.price} lei</span>
-	      						<span class="product-add-to-cart"><a href="#0" class="add-to-cart" data-id=${item.id}>Add+</a></span>
-      						</td>`;
-        if(tableItems){
+                                <span class="product-name">${item.name}</span>
+                                <span class="product-description">${item.description}</span>
+                            </td>
+                            <td>
+                                <span class="product-price">${item.price} lei</span>
+                                <span class="product-add-to-cart"><a href="#0" class="add-to-cart" data-id=${item.id}>Add+</a></span>
+                            </td>`;
+        if (tableItems) {
             tableItems.appendChild(productEl);
-        }
-        
-        
-        // switch (item.category){
-        //     case "paste":
-        //         tablePasta.appendChild(productEl);
-        //         break;
-        //     case "burgeri":
-        //         tableBurgers.appendChild(productEl);
-        //         break;
-        //     case "ciorbe":
-        //         tableSoup.appendChild(productEl);
-        //         break;
-        //     case "meniu":
-        //         tableMenu.appendChild(productEl);
-        //         break;
-        //     case "salate":
-        //         tableSalad.appendChild(productEl);
-        //         break;
-        //         default:
-        //          tableItems.appendChild(productEl);
 
-        // }
+            switch (item.category) {
+                case "paste":
+                    tablePasta.appendChild(productEl);
+                    break;
+                case "burgeri":
+                    tableBurgers.appendChild(productEl);
+                    break;
+                case "ciorbe":
+                    tableSoup.appendChild(productEl);
+                    break;
+                case "meniu":
+                    tableMenu.appendChild(productEl);
+                    break;
+                case "salate":
+                    tableSalad.appendChild(productEl);
+                    break;
+                default:
+                    tableItems.appendChild(productEl);
+
+            }
+
+        }
     });
 }
 
 
 var generateCartList = function() {
     orderList.innerHTML = "";
-    productsInCart.forEach(function(item,index) {
+    productsInCart.forEach(function(item, index) {
         var tr = document.createElement("tr");
         tr.classList.add('order-box_item');
 
@@ -184,13 +199,13 @@ var setupListeners = function() {
         console.log(productsInCart);
 
     });
-    tableCart.addEventListener('click', function(event){
-      var btnRem = event.target;
-      if(btnRem.classList.contains("remove")){
-        var btnRemId = btnRem.dataset.id;
-        console.log("Button target: " + btnRemId);
-        removeItemFromCart(btnRemId);
-      }
+    tableCart.addEventListener('click', function(event) {
+        var btnRem = event.target;
+        if (btnRem.classList.contains("remove")) {
+            var btnRemId = btnRem.dataset.id;
+            console.log("Button target: " + btnRemId);
+            removeItemFromCart(btnRemId);
+        }
     });
     emptyCartEl.addEventListener("click", function(event) {
         if (confirm("Are you sure?")) {
@@ -198,26 +213,45 @@ var setupListeners = function() {
         }
         generateCartList();
     });
-    send.addEventListener("click", function(e){
+    send.addEventListener("click", function(e) {
         e.preventDefault();
-        var message = "Comanda a fost inregistrata cu succes!";
-        sendOrder();
-        if( alert(message)){
-            productsInCart = [];
+        if (productsInCart.length >= 1) {
+            sendOrder();
+            var cont = document.querySelector('.container-content_right');
+            var divSuccess = document.createElement('div');
+            var message = `<p class="success">Comanda a fost inregistrata cu succes!</p> `;
+            divSuccess.innerHTML = message;
+            storeMyOrder();
         }
+        cont.appendChild(divSuccess);
+        productsInCart = [];
+
         generateCartList();
-        
+
     });
 
 }
-var sendOrder = function(){
+var sendOrder = function() {
     var city = document.getElementById('selectCity').value;
     var address = document.getElementById('address').value;
     var phone = document.getElementById('phone').value;
-    var orderProducts = productsInCart.map(function(item,index){ return productsInCart[index]} );
-    if(productsInCart.length >= 1){
-        orderStored.push({"city": city, "address": address, "phone":phone, "order": orderProducts});
-    }
+    var orderProducts = productsInCart.map(function(item, index) {
+        return productsInCart[index]
+    });
+    console.log(orderProducts);
+    orderStored.push({
+        "city": city,
+        "address": address,
+        "phone": phone,
+        "order": orderProducts
+    });
+    localStorage.setItem("order", JSON.stringify(orderStored));
+
+}
+var storeMyOrder = function() {
+    var getOrder = localStorage.getItem('order');
+    var displayConfirmOrder = JSON.parse(getOrder);
+    console.log(displayConfirmOrder);
 }
 
 var addToCart = function(id) {
@@ -238,14 +272,14 @@ var addToCart = function(id) {
     }
     generateCartList();
 }
- var removeItemFromCart = function(itemId){
-     if(productsInCart[itemId].quantity === 1){
-        productsInCart.splice(itemId,1);
-     }else{
-      productsInCart[itemId].quantity--;
-     }
-      generateCartList();
- }
+var removeItemFromCart = function(itemId) {
+    if (productsInCart[itemId].quantity === 1) {
+        productsInCart.splice(itemId, 1);
+    } else {
+        productsInCart[itemId].quantity--;
+    }
+    generateCartList();
+}
 var productFound = function(productId) {
     return productsInCart.find(function(item) {
         return item.product.id === productId;
@@ -257,28 +291,33 @@ var calculateTotalPrice = function() {
         return total + (item.product.price * item.quantity);
     }, 0);
 }
-var generateRestaurants = function(){
+var generateRestaurants = function() {
 
 }
-var restaurantsCateg = function(){
+
+var restaurantsCateg = function() {
     products.forEach(function(item) {
-    var restaurantEl = document.createElement('div');
-    restaurantEl.classList.add('boxes_item','boxes_item--indian');
-    restaurantEl.innerHTML = `<div class="boxes_item-content">
-                                    <a href="order.html" class="link"><span class="link-text">${item.restaurant}</span> <i class="fas fa-arrow-right"></i></a>
+        var restaurantEl = document.createElement('div');
+        restaurantEl.classList.add('boxes_item', 'boxes_item--indian');
+        restaurantEl.innerHTML = `<div class="boxes_item-content">
+                                    <a href="order.html?page=${item.restaurant}" class="link"><span class="link-text">${item.restaurant}</span> <i class="fas fa-arrow-right"></i></a>
                                  </div>`;
-        if(restaurantEl){boxes.appendChild(restaurantEl);}
-        
+        //boxes.appendChild(restaurantEl);
+
+        if (boxes) {
+            boxes.appendChild(restaurantEl);
+        }
     });
 }
+
 
 
 // search
 
 // var searchInput = document.querySelector('.search input').value;
 
-var findRestaurants = function(myRestaurants, title){
-    var titleReturned = myRestaurants.find(function(item, index){
+var findRestaurants = function(myRestaurants, title) {
+    var titleReturned = myRestaurants.find(function(item, index) {
         return item.restaurant.toLowerCase() === title.toLowerCase();
     });
     return titleReturned;
@@ -293,12 +332,27 @@ var findRestaurants = function(myRestaurants, title){
 //     restaurantsCateg();
 // });
 
+
+
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+        vars[key] = value;
+
+    });
+    return vars.page;
+}
+console.log(getUrlVars());
+var pageTitle = document.querySelector('.container-title > h1');
+if (getUrlVars()) {
+    pageTitle.textContent = getUrlVars();
+}
+
 var init = function() {
     generateProductList();
-    
-    setupListeners();
     restaurantsCateg();
+    setupListeners();
+
 }
 init();
-
-
